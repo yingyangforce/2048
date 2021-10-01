f3bd7b753cf988444903129ca1b22ed5d9e1df6e
Gene Calmer <yingyangforce@gmail.com>
2021-10-01 10:34:38 -0400

    Tile updateYCords now works

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -107,10 +107,68 @@ function updateXCords(xdir) {
 
 //tileArr arrays should be same length, take indexes from first array
 function updateYCords(ydir) {
+    let anythingmoved = false;
+
     for (index in tileArr[0]) {
         //makes col array out of row arrs @ index index
-        const coltiles = tileArr.map(member => member[index]);
-        console.log(`Col Y-${index}`, coltiles.filter(member => member !== null));
+        const col = tileArr.map(member => member[index]);
+
+        if (col.every(member => member === null)) { continue; }
+
+        const coltiles = col.filter(member => member !== null);
+
+        if (ydir === 1) {
+            coltiles.reverse();
+        }
+
+        for (let i = 0; i < coltiles.length; i++) {
+            if (coltiles[i + 1]) {
+                if (coltiles[i].val === coltiles[i + 1].val) {
+                    coltiles[i].val *= 2;
+                    coltiles.splice(i + 1, 1);
+                }
+            }
+            if (ydir == 1) {
+                if (coltiles[i].y !== col.length - i - 1) {
+                    anythingmoved = true;
+                }
+                coltiles[i].updateY(col.length - i - 1);
+            } else {
+                if (coltiles[i].y !== col.length - i - 1) {
+                    anythingmoved = true;
+                }
+                coltiles[i].updateY(i);
+            }
+        }
+        
+        if (ydir === 1) {
+            coltiles.reverse();
+        }
+
+        const numnulls = col.length - coltiles.length;
+        
+        if (ydir === 1) {
+            for (let i = 0; i < numnulls; i++) {
+                coltiles.unshift(null);
+            }
+            console.log(`New col:${index}`, coltiles);
+        } else {
+            for (let i = 0; i < numnulls; i++) {
+                coltiles.push(null);
+            }
+            console.log(`New col:${index}`, coltiles);
+        }
+        //TODO: MAKE THIS WORK FOR INDEX OF ROW INSTEAD OF ROWS
+        //tileArr[tileArr.indexOf(row)] = rowtiles;
+        for (row in tileArr) {
+            tileArr[row][index] = coltiles[row];
+        }
+    }
+
+    if (anythingmoved) {
+        console.log("Something moved.");
+    } else {
+        console.log("Nothing moved.");
     }
 }
 
